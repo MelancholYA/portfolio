9a7b9f2fdc70ea4ea522a003ebad81958aa42f96
yacine ouardi <ouardi.yacin3@gmail.com>
2025-04-28 23:57:10 +0100

    feat(footer): add footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -97,6 +97,28 @@ export default function RootLayout({
             __html: JSON.stringify(schemaData),
           }}
         />
+        <footer className="py-8 border-t border-gray-100 hue">
+          <div className="container mx-auto px-4">
+            <div className="flex flex-col md:flex-row justify-between items-center">
+              <p className="text-sm text-gray-500">© 2025 Yacine Ouardi</p>
+              <div className="flex space-x-4 mt-4 md:mt-0">
+                <a
+                  href="https://github.com/melancholYA/"
+                  className="text-primary hover:text-white"
+                >
+                  GitHub
+                </a>
+
+                <a
+                  href="https://www.linkedin.com/in/yacine-ouardi/"
+                  className="text-primary hover:text-white"
+                >
+                  LinkedIn
+                </a>
+              </div>
+            </div>
+          </div>
+        </footer>
       </body>
     </html>
   );
