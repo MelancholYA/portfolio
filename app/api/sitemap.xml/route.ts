f4fc004b9d65adf002517fc81af296d81f3c21c0
yacine ouardi <ouardi.yacin3@gmail.com>
2025-05-03 14:42:32 +0100

    fix(seo): fix sitemap

--- app/api/sitemap.xml/route.ts
+++ app/api/sitemap.xml/route.ts
@@ -1,5 +1,3 @@
-// pages/api/sitemap.xml.ts (or /app/sitemap.xml/route.ts in App Router)
-
 import { client } from "../../../tools/sanity/client";
 
 const query = `*[_type == "post"]{ "slug": slug.current, _updatedAt }`;
@@ -7,25 +5,41 @@ const query = `*[_type == "post"]{ "slug": slug.current, _updatedAt }`;
 export async function GET() {
   const posts = await client.fetch(query);
 
-  const urls = posts
-    .map(
-      (post: { slug: string; _updatedAt: string }) => `
-      <url>
-        <loc>https://yacine-ouardi.vercel.app/blog/${post.slug}</loc>
-        <lastmod>${post._updatedAt.split("T")[0]}</lastmod>
-        <priority>0.8</priority>
-      </url>`
-    )
-    .join("");
-
-  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  const staticUrls = [
+    `
     <url>
       <loc>https://yacine-ouardi.vercel.app/</loc>
       <lastmod>${new Date().toISOString().split("T")[0]}</lastmod>
       <priority>1.0</priority>
+    </url>`,
+    `
+    <url>
+      <loc>https://yacine-ouardi.vercel.app/blog</loc>
+      <lastmod>${new Date().toISOString().split("T")[0]}</lastmod>
+      <priority>0.9</priority>
+    </url>`,
+    `
+    <url>
+      <loc>https://yacine-ouardi.vercel.app/blog/posts</loc>
+      <lastmod>${new Date().toISOString().split("T")[0]}</lastmod>
+      <priority>0.8</priority>
+    </url>`,
+  ];
+
+  const postUrls = posts.map(
+    (post: { slug: string; _updatedAt: string }) => `
+    <url>
+      <loc>https://yacine-ouardi.vercel.app/blog/posts/${post.slug}</loc>
+      <lastmod>${post._updatedAt.split("T")[0]}</lastmod>
+      <priority>0.7</priority>
     </url>
-    ${urls}
+  `
+  );
+
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    ${staticUrls.join("\n")}
+    ${postUrls.join("\n")}
   </urlset>`;
 
   return new Response(sitemap, {
